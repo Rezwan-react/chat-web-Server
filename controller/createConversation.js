674a67b4fbf06dde2553b2522c32f4a216061e02
Rezwan-react <mdrazwanislam8@gmail.com>
2025-04-21 20:44:21 +0600

    createConversation

--- controller/createConversation.js
+++ controller/createConversation.js
@@ -1,5 +1,24 @@
-const createConversation = (req, res) => {
-    res.send("createConversation")
+const conversationSchema = require("../modal/conversationSchema");
+const userSchema = require("../modal/userSchema");
+
+const createConversation = async (req, res) => {
+
+    try {
+        const { participentEmail } = req.body;
+
+        const participentData = await userSchema.findOne({ email: participentEmail })
+        if (!participentData) {
+            return res.status(400).send({ error: "user not found" })
+        }
+
+        const conversation = new conversationSchema({
+            creator: req.user.id,
+            participent: participentData._id
+        })
+
+    } catch (error) {
+        res.status(500).send("Server error")
+    }
 }
 
-module.exports = {createConversation}
+module.exports = { createConversation }
